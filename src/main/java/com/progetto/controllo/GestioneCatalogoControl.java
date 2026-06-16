e7b09dd470cb3c4c50963ebfeb011d20e9000b12
andrea <andrea.gargiulo2006@gmail.com>
2026-06-16 16:46:47 +0200

    FIX GENERALI 10

--- src/main/java/com/progetto/controllo/GestioneCatalogoControl.java
+++ src/main/java/com/progetto/controllo/GestioneCatalogoControl.java
@@ -1,6 +1,7 @@
 package com.progetto.controllo;
 
 import java.time.Year;
+import java.time.ZoneId; // FIX S8688: Importiamo il fuso orario
 
 import com.progetto.database.VideogiocoDAO;
 import com.progetto.entita.Videogioco;
@@ -20,7 +21,8 @@ public class GestioneCatalogoControl {
         // Controllo anno valido
         try {
             int anno = Integer.parseInt(annoString);
-            if (anno < 1950 || anno > Year.now().getValue() + 5) return false;
+            // FIX S8688: Dichiariamo esplicitamente il fuso orario di sistema
+            if (anno < 1950 || anno > Year.now(ZoneId.systemDefault()).getValue() + 5) return false;
             
             // Creazione e salvataggio
             Videogioco nuovoGioco = new Videogioco(titolo, genere, anno, dev, desc);
